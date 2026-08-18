fa0f259669a847172181deeb9d1dee3c54571ce4
Sameer Aly <sameeraly240@gmail.com>
2026-08-18 22:24:22 +0500

    origin

--- for_loop.py
+++ for_loop.py
@@ -1,16 +1,29 @@
 
-gussedNumber=11
-i=10
-while i>=1:
-    x=int(input("enter a number :"))
-    if x==gussedNumber:
-        print("congratulation !🎉🎉 your guess is right🎉🎉")
+# gussedNumber=11
+# i=10
+# while i>=1:
+#     x=int(input("enter a number :"))
+#     if x==gussedNumber:
+#         print("congratulation !🎉🎉 your guess is right🎉🎉")
         
-        break
-    else:
-        print("Try again ") 
-        i -=1
-        print("you have ",i,"attempt left...")   
-print("you attempt is finish...")
-print("the guess number is ",gussedNumber)
+#         break
+#     else:
+#         print("Try again ") 
+#         i -=1
+#         print("you have ",i,"attempt left...")   
+# print("you attempt is finish...")
+# print("the guess number is ",gussedNumber)
+
+# for el in range(1,20,3):
+#     print(el)
+
+# n= int(input("enter a number :"))
+# for i in range(1,11):
+#     print(n,"*",i,"=",n*i)
+
+number=int(input("enter  a number :"))
+total=0
+for i in range(1,number+1):
+    total +=i
+print("the sum of first ",number,"number is :",total)
 
