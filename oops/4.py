392af7b428725584f4595881ac13b18b18a03b8f
Sameer Aly <sameeraly240@gmail.com>
2026-08-24 21:26:29 +0500

    Fix syntax error in 4.py (closes #1)

--- oops/4.py
+++ oops/4.py
@@ -1,11 +1,10 @@
 class Account:
-    def __init__(self,acc_No,pas):
-        self__AccountNo=acc_No
-        self__Password=pas
-    
+    def __init__(self, acc_No, pas):
+        self.__AccountNo = acc_No  # Added dot (.) here
+        self.__Password = pas      # Added dot (.) here
 
     def resetPassword(self):
         print(self.__AccountNo) 
-    
-A1=Account("12344","abcd")  
-print(A1.resetPassword() )
+
+A1 = Account("12344", "abcd")
+A1.resetPassword()  # Called directly without wrapping in print()
